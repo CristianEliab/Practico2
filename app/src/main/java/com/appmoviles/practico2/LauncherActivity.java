c1bcbe03f2f7cba98cbff8b98cc95414000d4c26
cristianeliab <eliadcrist@gmail.com>
2019-05-02 15:28:59 -0500

    first commit

---
+++ app/src/main/java/com/appmoviles/practico2/LauncherActivity.java
@@ -0,0 +1,39 @@
+package com.appmoviles.practico2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    private Button launcher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        init();
+    }
+
+    private void init() {
+        launcher = findViewById(R.id.btn_launcher);
+        launcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LauncherActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+
+
+}
